ca613909ad07cbf9c4185c207e89907910b65883
Rodolfo Santos <rodolfodossantos29@gmail.com>
2022-05-08 09:43:36 -0300

    🎉 create project

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <Nav />
+    <div class="container">
+      <h1>Welcome to Restaurantly</h1>
+      <a href="/restaurant">Go to restaurants</a>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+</style>
---
+++ pages/restaurants.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup></script>
+
+<template>
+  <Nav />
+  <h1>Hello this is the restaurants</h1>
+</template>
+
+<style scoped></style>
