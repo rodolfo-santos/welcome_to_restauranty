06221be380db858cc33c6ed85ab1ec2dbec9111a
Rodolfo Santos <rodolfodossantos29@gmail.com>
2022-05-08 10:35:03 -0300

    ✨ create layouts

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-center">
+    <slot name="header"></slot>
+    <div>
+      <img src="https://www.iconpacks.net/icons/2/free-sad-face-icon-2691-thumb.png" alt="" />
+    </div>
+    <slot name="redirectEl" />
+  </div>
+</template>
+
+<style scoped>
+img {
+  width: 10rem;
+}
+</style>
